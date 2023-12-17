1db0d63e2be1f4102995783df867ee06a7002425
Wagner Aquino <wcaquino@gmail.com>
2023-12-17 17:22:15 -0600

    fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    "arrow-parens": ["error", "as-needed"],
+    "object-curly-newline": ["error", {"multiline": true}]
+  },
+};
--- src/app.js
+++ src/app.js
@@ -45,10 +45,4 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// app.db.on('query', (query) => {
-//   console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : '' });
-// })
-//   .on('query-response', response => console.log(response))
-//   .on('error', error => console.log(error));
-
 module.exports = app;
